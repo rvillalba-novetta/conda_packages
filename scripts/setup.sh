2ec5ca16673b8d794a73b8302f282146424adf55
Roberto <rvillalba@novetta.com>
2018-06-26 18:29:32 +0000

    Add conda-forge to setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,5 +4,8 @@ set -e
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.."; pwd)"
 
-# Add channel to conda
+# Add channels to conda
+if ! conda config --get channels | grep conda-forge; then
+	conda config --add channels conda-forge
+fi
 conda config --add channels $ROOT_DIR/pkgs/channel/linux-64
